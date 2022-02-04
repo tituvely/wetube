ced1ca7a71843ff9705058cebf5b47e2a8130db2
Yookyung Kim <tituvely@gmail.com>
2022-02-04 19:51:44 +0900

    middleware - app.use

--- src/server.js
+++ src/server.js
@@ -4,16 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log("I'm in the middle!");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("Not Allowed");
+  }
+  console.log("Allowed, you may continue");
   next();
 };
 
 const handleHome = (req, res) => {
-  return res.end();
+  return res.send("HOME");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
