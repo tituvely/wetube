dc0448c8ea9e9ef795f2252b851133a9c7f49ec1
Yookyung Kim <tituvely@gmail.com>
2022-02-04 19:29:09 +0900

    get request

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
