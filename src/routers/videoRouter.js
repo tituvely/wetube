6f3b87b1df9fd7ad7bebf547f981da2b87fd4598
Yookyung Kim <tituvely@gmail.com>
2022-02-14 17:04:39 +0900

    숫자 정규식

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,7 +4,7 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
+videoRouter.get("/:id(\\d+)", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
 
