af400ad64226524506830975f7185e41e86e5d59
Yookyung Kim <tituvely@gmail.com>
2022-02-14 21:58:18 +0900

    Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,9 @@ export const trending = (req, res) => {
 export const see = (req, res) => {
   return res.render("watch");
 };
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => {
+  return res.render("edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
