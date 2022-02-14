56f46b6ad48db8ceb01c05e854ca7123ad1f8e84
Yookyung Kim <tituvely@gmail.com>
2022-02-14 22:33:10 +0900

    conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,12 @@
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "Daisy",
+  loggedIn: true,
 };
-export const see = (req, res) => {
-  return res.render("watch");
-};
-export const edit = (req, res) => {
-  return res.render("edit");
-};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
