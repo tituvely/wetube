784308483bdc437dfc3e5dfe7ce3f1c245ac3ad1
Yookyung Kim <tituvely@gmail.com>
2022-02-14 17:02:42 +0900

    router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { edit, remove } from "../controllers/userController";
+import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
+
+userRouter.get(":id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/logout", logout);
 
 export default userRouter;
